1fe933733dea08a86ac5ee80b5b2795d8c098f41
Jakob Helgesson <jakob.helgesson@gmail.com>
2025-05-05 13:26:39 +0200

    Add scrollFix to the mixin

--- src/main/java/me/virb3/mcmouser/mixin/MixinMouse.java
+++ src/main/java/me/virb3/mcmouser/mixin/MixinMouse.java
@@ -4,6 +4,7 @@ import net.minecraft.client.MouseHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(MouseHandler.class)
 public class MixinMouse {
@@ -11,4 +12,10 @@ public class MixinMouse {
     private boolean onPress() {
         return false;
     }
+
+    //Doubles and longs are counted twice, index 5 is vertical in onScroll.
+	@ModifyVariable(method = "onScroll", at = @At(value = "HEAD", target = "Lnet/minecraft/client/Minecraft;ON_OSX:Z"), index = 5)
+	private double scrollFix(double vertical1, long window, double horizontal, double vertical2) {
+		return vertical1 == 0 ? horizontal : vertical1;
+	}
 }
